8008a60ac5c7110ee8c49168f027f627506de8f3
Gourav10yrs <gourav.agarwal569@gmail.com>
2021-10-16 23:18:51 +0530

    fixed the heroone box movement

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {World, O3D }from './components/Background';
 
 
 const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2]
-const trans1 = (x, y) => `translate3d(${x / 25}px,${y / 25}px,0)`
+const trans1 = (x, y) => `translate3d(${x / 10}px,${y / 10}px,0)`
 
 function App() {
   const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 550, friction: 140 } }))  
